da2cb8ce9322eaef6ff120b8628268899d0384a2
viviana <vivianavelasquez11@gmail.com>
2024-07-15 22:18:33 -0500

    feat: project initialization

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+lambda_function_name     = "studentCrudLambda"
+s3_lambda_function_name  = "s3FileRegisterLambda"
+dynamodb_table_name      = "studentTable"
+s3_bucket_name           = "studentUploadsS3Bucket"
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "lambda_function_name" {
+  description = "Lambda function name for CRUD"
+  type        = string
+  default     = "studentCrudLambdaFunction"
+}
+
+variable "s3_lambda_function_name" {
+  description = "Lambda function name for S3"
+  type        = string
+  default     = "s3RegisterLambdaFunction"
+}
+
+variable "dynamodb_table_name" {
+  description = "DynamoDB table name"
+  type        = string
+  default     = "students-table"
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "student-uploads-bucket"
+}
